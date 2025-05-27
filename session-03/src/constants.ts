5865de1ef05d1372575e4067445aca9b8b2995d2
Sagi Weizmann <sagiweizmann@gmail.com>
2025-05-27 12:54:00 +0300

    Blob implementation and meta data of the blob , POST GET DELETE

---
+++ session-03/src/constants.ts
@@ -0,0 +1,6 @@
+export const MAX_LENGTH = 10 * 1024 * 1024; // 10MB
+export const MAX_DISK_QUOTA = 1 * 1024 * 1024 * 1024; // 1GB
+export const MAX_HEADER_LENGTH = 50;
+export const MAX_HEADER_COUNT = 20;
+export const MAX_ID_LENGTH = 200;
+export const MAX_BLOBS_IN_FOLDER = 10000;
